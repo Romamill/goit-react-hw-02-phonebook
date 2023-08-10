65d0ec1d2ff55edc03c31ed851ff956feffc0059
Romamill <122994080+Romamill@users.noreply.github.com>
2023-08-10 12:22:50 +0300

    update pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -30,7 +30,7 @@ class ContactForm extends Component {
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я\s]*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces."
             required
           />
@@ -44,7 +44,7 @@ class ContactForm extends Component {
             name="number"
             value={this.state.number}
             onChange={this.handleChange}
-            pattern="[0-9\s-+()]*"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
